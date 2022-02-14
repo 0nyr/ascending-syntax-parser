10902020b10721571a6be4562d74d423a633b33c
onyr <rascoussier.florian@gmail.com>
2022-02-14 20:01:12 +0100

    Minor corrections. Trying to make things get compiled.

--- src/state/I0.cpp
+++ src/state/I0.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#include "inc/exception/NoActionException.hpp"
 #include "inc/state/State.hpp"
+#include "inc/exception/NoActionException.hpp"
 
 bool I0::Action(Symbol* a)
 {
